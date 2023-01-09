8d2bababce7b52bdc65c751df0e98afcab39f486
amrit-911 <92154124+amrit-911@users.noreply.github.com>
2023-01-09 15:12:18 +0530

    A basic counter app

---
+++ script.js
@@ -0,0 +1,13 @@
+let button1 = document.querySelector(".button1");
+let button2 = document.querySelector(".button2");
+let reset = document.querySelector(".reset");
+let head = document.querySelector("h1");
+let i = 0;
+button1.addEventListener("click", () => {
+    head.innerHTML = ++i;
+});
+button2.addEventListener("click", () => { head.innerHTML = --i; });
+reset.addEventListener("click", () => {
+    head.innerHTML = 0;
+    i = 0;
+})
